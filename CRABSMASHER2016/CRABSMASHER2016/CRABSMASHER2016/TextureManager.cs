f79435eb1455c87684146a3b0bd7fe78ad807a34
Kristoffer Franzon <Driverinput@gmail.com>
2015-04-09 10:08:45 +0200

    texturemanager

---
+++ CRABSMASHER2016/CRABSMASHER2016/CRABSMASHER2016/TextureManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace game
+{
+    public class TextureManager
+    {
+        public static Dictionary<string, string> InitializeTextures = new Dictionary<string, string>();
+        public static Dictionary<string, Texture2D> Textures = new Dictionary<string, Texture2D>();
+        
+        public static void LoadContent(ContentManager cm)
+        {
+            foreach (KeyValuePair<string, string> item in InitializeTextures)
+            {
+                Textures.Add(item.Key, cm.Load<Texture2D>(item.Value));
+            }
+            InitializeTextures = null;
+        }
+    }
+}
